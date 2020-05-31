63cc9efde13ad39843e46c1fd9177b0dc4880ba7
Oscar Funes <me@oscarfunes.com>
2020-05-30 23:24:38 -0700

    use minimist

---
+++ minimist/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * trying to understand what is happening in confit
+ * https://github.com/krakenjs/confit/blob/3.x/lib/provider.js#L29
+ */
+
+const Hoek = require('@hapi/hoek');
+const minimist = require('minimist');
+
+const args = minimist(process.argv.slice(2));
+
+for (let key of Object.keys(args)) {
+  console.log(key, args[key]);
+}
+
+// node minimist --xyz=asdf --x.y.0.a 123
+
+const defaults = {
+  x: {
+    y: [
+      {
+        a: 'default'
+      }
+    ]
+  }
+}
+
+const result = Hoek.applyToDefaults(defaults, args);
+
+console.log({ result });
